b84986ce342f727bd0d87d1a8ea61dc99785ebf4
Ruslan Salkaev <rsalkaev@gmail.com>
2022-04-11 21:08:41 +0300

    Add topics as ColoredFragment

--- src/main/kotlin/org/jetbrains/topvisplugin/decorators/TopicsDecorator.kt
+++ src/main/kotlin/org/jetbrains/topvisplugin/decorators/TopicsDecorator.kt
@@ -1,13 +1,18 @@
 package org.jetbrains.topvisplugin.decorators
 
-import org.jetbrains.topvisplugin.settings.TopvisPluginSettings
-import org.jetbrains.topvisplugin.util.TopicsParser
 import com.intellij.ide.projectView.PresentationData
 import com.intellij.ide.projectView.ProjectViewNode
 import com.intellij.ide.projectView.ProjectViewNodeDecorator
+import com.intellij.ide.util.treeView.NodeRenderer.getSimpleTextAttributes
+import com.intellij.ide.util.treeView.PresentableNodeDescriptor
 import com.intellij.openapi.components.service
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.packageDependencies.ui.PackageDependenciesNode
 import com.intellij.ui.ColoredTreeCellRenderer
+import com.intellij.ui.SimpleTextAttributes
+import com.intellij.util.ui.UIUtil
+import org.jetbrains.topvisplugin.settings.TopvisPluginSettings
+import org.jetbrains.topvisplugin.util.TopicsParser
 
 
 class TopicsDecorator : ProjectViewNodeDecorator {
@@ -23,7 +28,27 @@ class TopicsDecorator : ProjectViewNodeDecorator {
                 val topicsInfo = projectPath?.let { virtualFile.path.removePrefix(it) }
                     ?.let { TopicsParser.getTopicsInfo(it) }
                 if (topicsInfo != null && data != null) {
-                    data.locationString = topicsInfo
+                    val text = data.presentableText
+                    val attributes = getSimpleTextAttributes(data)
+                    data.addText(PresentableNodeDescriptor.ColoredFragment(text, attributes))
+                    val location: String? = data.locationString
+                    if (!StringUtil.isEmpty(location)) {
+                        val attributesWithLocation =
+                            SimpleTextAttributes.merge(attributes, SimpleTextAttributes.GRAYED_ATTRIBUTES)
+                        data.addText(
+                            PresentableNodeDescriptor.ColoredFragment(
+                                data.locationPrefix + location + data.locationSuffix,
+                                attributesWithLocation
+                            )
+                        )
+                    }
+                    data.addText(
+                        PresentableNodeDescriptor.ColoredFragment(
+                            " $topicsInfo",
+                            SimpleTextAttributes(SimpleTextAttributes.STYLE_PLAIN, UIUtil.getInactiveTextColor())
+                        )
+                    )
+                    // data.locationString = topicsInfo
                 }
             }
         }
